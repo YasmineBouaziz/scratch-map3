a35772eaacdd43337ee40cbfd31fb69708be9b74
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-12-18 14:29:15 +0000

    overrided bootstrap scss

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,27 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from './map_icon.png';
+import './_Navbar.scss';
 
 function NavBar() {
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg">
       <Container>
       <Navbar.Brand href="#home">
-          <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
           </Navbar.Brand>
           <Navbar.Brand> Scratch Map
           </Navbar.Brand>
+      {/* </Navbar.Brand> */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Home</Nav.Link>
-            <Nav.Link href="#features">Places I've been</Nav.Link>
-            <Nav.Link href="#features">Places I would like to go</Nav.Link>
+            <Nav.Link href="#features">Map</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#deets">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
