5382c65a0bfa3dcf2ec61227ae9c15ec16003058
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-12-31 19:27:16 +0000

    Populate about page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { lazy } from 'react'
-import './style/App.scss'
-import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './style/map.scss'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import "./style/App.scss";
+import "./style/map.scss";
 
-const About = lazy(() => import('./components/About'))
-const Home = lazy(() => import('./components/map'))
-
-function App () {
+function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
       <Router>
         <Routes>
-          <Route path='/about' element={<About />} />
-          <Route path='/' element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-import logo from './map_icon.png'
+import logo from './images/map_icon.png'
 
 function NavBar () {
   return (
