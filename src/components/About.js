1dd0a8c551325cb844a94a907df4c7bcc485ac38
Jays-code-collection <63003326+Jays-code-collection@users.noreply.github.com>
2023-01-01 18:26:31 +0000

    Fixup linting (#11)

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function About () {
+function About() {
   return (
     <div>
       <h1>Welcome to Scratch Map</h1>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import logo from './map_icon.png'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import logo from "./map_icon.png";
 
-function NavBar () {
+function NavBar() {
   return (
     <>
-      <Navbar collapseOnSelect expand='lg' bg='light' variant='light'>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
         <Container>
-          <Navbar.Brand href='/'>
+          <Navbar.Brand href="/">
             {/*
               <Link to="/home">
               </Link> */}
-            <img src={logo} className='App-logo' alt='logo' />
+            <img src={logo} className="App-logo" alt="logo" />
           </Navbar.Brand>
-          <Navbar.Brand href='/'> Scratch Map
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-          <Navbar.Collapse id='responsive-navbar-nav'>
+          <Navbar.Brand href="/"> Scratch Map</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
-              <Nav.Link href='/'>Home
-              </Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href='/about'>About
-              </Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
